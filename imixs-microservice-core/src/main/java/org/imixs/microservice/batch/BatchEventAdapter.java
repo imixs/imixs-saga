0c643ca309f502e1d64e57115a951892e58c057b
Ralph Soika <ralph.soika@imixs.com>
2019-11-03 18:52:32 +0100

    first draft
    Issue #57

---
+++ imixs-microservice-core/src/main/java/org/imixs/microservice/batch/BatchEventAdapter.java
@@ -0,0 +1,69 @@
+package org.imixs.microservice.batch;
+
+import java.util.logging.Logger;
+
+import javax.inject.Inject;
+
+import org.imixs.workflow.GenericAdapter;
+import org.imixs.workflow.ItemCollection;
+import org.imixs.workflow.engine.EventLogService;
+import org.imixs.workflow.engine.WorkflowService;
+import org.imixs.workflow.exceptions.AdapterException;
+import org.imixs.workflow.exceptions.PluginException;
+
+/**
+ * This generic adapter class creates a batch event based on the current event
+ * workflow result.
+ * 
+ * Example:
+ * 
+ * <code>
+			<item name="batch.event.id">[EVENT_ID]</item>
+   </code>
+ * 
+ * The batch.event.id is the next bpmn event fired asynchrony by the
+ * BatchEventProcessor
+ * 
+ * @author rsoika
+ *
+ */
+public class BatchEventAdapter implements GenericAdapter {
+
+	public static final String BATCH_EVENT_ID = "batch.event.id";
+
+	@Inject
+	WorkflowService workflowService;
+
+	@Inject
+	EventLogService eventLogService;
+
+	private static Logger logger = Logger.getLogger(BatchEventAdapter.class.getName());
+
+	@Override
+	public ItemCollection execute(ItemCollection document, ItemCollection event) throws AdapterException {
+		try {
+
+			// Check the txtActivityResult for a batch-event
+			ItemCollection evalItemCollection = workflowService.evalWorkflowResult(event, document);
+			if (evalItemCollection == null) {
+				return document;
+			}
+
+			// test for a batch event ....
+			if (evalItemCollection.hasItem(BATCH_EVENT_ID)) {
+				int batchEventID = evalItemCollection.getItemValueInteger(BATCH_EVENT_ID);
+				logger.info("...create new batch event - eventId=" + batchEventID);
+				// ceate EventLogEntry....
+				ItemCollection batchData = new ItemCollection().event(batchEventID);
+				eventLogService.createEvent(BatchEventProcessor.EVENTLOG_TOPIC_BATCH_EVENT, document.getUniqueID(),
+						batchData);
+			}
+
+		} catch (PluginException e) {
+			throw new AdapterException(e.getErrorContext(), e.getErrorCode(), e.getMessage(), e);
+		}
+
+		return document;
+	}
+
+}
